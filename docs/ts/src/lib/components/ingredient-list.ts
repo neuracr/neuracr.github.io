ce278acd39dddb1eceabb9c3c5d2fad4f348af33
Hsb511 <hugo-selle@hotmail.fr>
2021-12-28 20:36:01 +0100

    README updated + Changing the servings input

--- docs/ts/src/lib/components/ingredient-list.ts
+++ docs/ts/src/lib/components/ingredient-list.ts
@@ -54,15 +54,37 @@ export class IngredientList extends LitElement {
     servingNumber = 5;
 
     static styles = css`
-        #servinginput,label{
-            margin-left: 60px;
+        label {
+            margin: 0 60px;
+        }
+
+        button {
+            border: none;
+            font-size: 16px;
+        } 
+
+        #servinginput {
+            border: none;
+            text-align:center;
+        }
+
+        #servinginput::-webkit-outer-spin-button,
+        #servinginput::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        #servinginput[type=number] {
+            -moz-appearance: textfield;
         }
     `;
 
     render() {   
         return html`
         <label for="servinginput">Nb of servings</label>
+        <button  @click=${this.removeServings}>-</button>
         <input type="number" @input=${this.updateServings} id="servinginput" value=${this.servingNumber}>
+        <button @click=${this.addServings}>+</button>        
         <ul>
         <slot id="ingredientslot"></slot>
         </ul>
@@ -84,5 +106,23 @@ export class IngredientList extends LitElement {
 
         const ingredientItems = this.ingredientslot.assignedElements();
         ingredientItems.map(n => {n.setAttribute('currentServings', "" + this.servingNumber)});
+    }
+
+    removeServings() {
+        if (Number.isNaN(parseInt(this.input.value))) { 
+            return;
+        }
+
+        this.input.value = (parseInt(this.input.value) - 1).toString();
+        this.updateServings();
+    }
+
+    addServings() {
+        if (Number.isNaN(parseInt(this.input.value))) { 
+            return;
+        }
+
+        this.input.value = (parseInt(this.input.value) + 1).toString();
+        this.updateServings();
     }
 }
