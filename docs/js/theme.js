099457ccead4e856a1ab4412c91ec8517da88459
Hsb511 <hugo-selle@hotmail.fr>
2022-01-16 19:12:23 +0100

    Storaging the current theme in localstorage

--- docs/js/theme.js
+++ docs/js/theme.js
@@ -1,21 +1,34 @@
-var currentTheme = 0;
+const themesList = ['neuracr', 'dark', 'maua', 'azgheda'];
+const cssVarList = ['primary-color', 'accent-color', 'background-color', 'dark-background-color', 'font-color', 'light-font-color', 'main-font-family', 'title-font-family'];
+const currentThemeStr = localStorage.getItem('currentTheme');
+if (!isNaN(currentThemeStr)) {
+    setTheme(currentThemeStr);
+}
 
-function switchTheme() {
-    const themesList = ['neuracr', 'dark', 'maua', 'azgheda'];
-    const cssVarList = ['primary-color', 'accent-color', 'background-color', 'dark-background-color', 'font-color', 'light-font-color', 'main-font-family', 'title-font-family'];
-    
-
+function setTheme(themePosition) {
     cssVarList.forEach(function(cssVar){
         document.documentElement.style.setProperty(
             '--' + cssVar,
             getComputedStyle(document.documentElement).getPropertyValue(
-                '--' + themesList[currentTheme + 1] + '-' + cssVar
+                '--' + themesList[themePosition] + '-' + cssVar
             )
         );
     });
+}
+
+function switchTheme() {
+    const currentThemeStr = localStorage.getItem('currentTheme');
+    var currentTheme = 0;
+    if (!isNaN(currentThemeStr)) {
+        currentTheme = parseInt(currentThemeStr);
+    }
+    
+    setTheme(currentTheme + 1);
+
     if (currentTheme < themesList.length) {
         currentTheme = currentTheme + 1;
     } else {
         currentTheme = 0;
     }
+    localStorage.setItem('currentTheme', currentTheme);
 }
